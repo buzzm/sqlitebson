1ab8a0ad55e2872dd44de4e3ea2cb955b4d5dedd
buzzm <b_moschetti@yahoo.com>
2023-01-20 08:43:02 -0500

    Minor edits

--- example1.c
+++ example1.c
@@ -1,3 +1,14 @@
+// Copyright (c) 2022  Buzz Moschetti <buzz.moschetti@gmail.com>
+// 
+// Permission to use, copy, modify, and distribute this software and its documentation for any purpose, without fee, and without a written agreement is hereby granted,
+// provided that the above copyright notice and this paragraph and the following two paragraphs appear in all copies.
+// 
+// IN NO EVENT SHALL THE AUTHOR BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, 
+// ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF THE AUTHOR HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// THE AUTHOR SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+// THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE AUTHOR HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
 #include <stdio.h>
 
 #include <sqlite3.h>
@@ -168,8 +179,8 @@ int main(int argc, char* argv[]) {
 
     // If you don't want to keep blasting the DB, you can comment
     // these out:
-    //create(db);
-    //for(int i = 0; i < 3; i++) { insert(db, i); }
+    create(db);
+    for(int i = 0; i < 3; i++) { insert(db, i); }
 
     // Returns binary BLOB
     do_fetch(db, "select bdata from FOO");
